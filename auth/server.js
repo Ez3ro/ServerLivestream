ee53a087e62438c2983bb444221faa8eb2727347
Ezekhiel <ezekhiel1337@gmail.com>
2023-05-04 17:35:53 +0800

    Livestreamserver-upload
    Upload live stream

---
+++ auth/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded());
+
+app.post("/auth", function (req, res) {
+    
+    const streamkey = req.body.key;
+
+    if (streamkey == "secretkey") {
+        res.status(200).send();
+        return;
+    }
+
+    res.status(403).send();
+
+
+
+
+});
+
+app.listen(8000, function (){
+    console.log("listening port 8000");
+
+});
